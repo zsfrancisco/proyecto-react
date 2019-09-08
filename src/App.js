7321a6fc0b51531ef41f9cd7ea63a444210feb1b
Francisco Javier Zambrano Santacruz <franciscozambrano@outlook.es>
2019-09-07 23:53:20 -0500

    commit clase 44 componente basado en clases

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+  
+    state = {
+      persons: [
+        { name: 'Francisco', age: 22},
+        { name: 'Jennifer', age: 21}
+      ],
+      otherState: 'Otro valor'
+    };
+
+    switchNameHandler = () => {
+      //console.log('was clicked!');
+      // NO HACER ESTO this.state.persons[0].name= 'Javier';
+      this.setState({
+        persons: [
+          { name: 'Javier', age: 22},
+          { name: 'Jennifer', age: 33}
+        ]
+      });
+    };
+
+    render() {
+      return (
+        <div className="App">
+          <h1>Aplicación REACT</h1>
+          <button onClick={this.switchNameHandler}>Cambiar nombre</button>
+          <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}>
+          </Person>  
+          <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}>
+            My hobbie es ir a correr en las mañanas
+          </Person>  
+        </div>
+      );
+// return React.createElement('div', {className: 'App'}, React.createElement('h1', null));
+    }
 }
 
 export default App;
